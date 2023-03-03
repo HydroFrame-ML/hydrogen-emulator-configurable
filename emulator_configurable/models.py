95dbfec3d239d1ea101b05573b3cd1a2fa61e612
arbennett <bennett.andr@gmail.com>
2023-03-03 18:25:13 -0500

    Add ForcedSTRNN structure

--- emulator_configurable/models.py
+++ emulator_configurable/models.py
@@ -15,6 +15,195 @@ import torch.nn as nn
 import torch.nn.functional as F
 from . import model_builder
 
+@model_builder.register_layer('ActionSTLSTMCell')
+class ActionSTLSTMCell(nn.Module):
+    def __init__(self, in_channel, action_channel, num_hidden, filter_size, stride):
+        super().__init__()
+
+        self.num_hidden = num_hidden
+        self.padding = filter_size // 2
+        self._forget_bias = 1.0
+        self.conv_x = nn.Conv2d(in_channel,     num_hidden * 7, filter_size, stride, self.padding)
+        self.conv_a = nn.Conv2d(action_channel, num_hidden * 4, filter_size, stride, self.padding)
+        self.conv_h = nn.Conv2d(num_hidden,     num_hidden * 4, filter_size, stride, self.padding)
+        self.conv_m = nn.Conv2d(num_hidden,     num_hidden * 3, filter_size, stride, self.padding)
+        self.conv_o = nn.Conv2d(num_hidden * 2, num_hidden,     filter_size, stride, self.padding)
+        self.conv_last = nn.Conv2d(num_hidden * 2, num_hidden, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x_t, a_t, h_t, c_t, m_t):
+        x_conv = self.conv_x(x_t)
+        a_conv = self.conv_a(a_t)
+        h_conv = self.conv_h(h_t)
+        m_conv = self.conv_m(m_t)
+        i_x, f_x, g_x, i_xp, f_xp, g_xp, o_x = torch.split(x_conv, self.num_hidden, dim=1)
+        i_h, f_h, g_h, o_h = torch.split(h_conv * a_conv, self.num_hidden, dim=1)
+        i_m, f_m, g_m = torch.split(m_conv, self.num_hidden, dim=1)
+
+        i_t = torch.sigmoid(i_x + i_h)
+        f_t = torch.sigmoid(f_x + f_h + self._forget_bias)
+        g_t = torch.tanh(g_x + g_h)
+
+        i_tp = torch.sigmoid(i_xp + i_m)
+        f_tp = torch.sigmoid(f_xp + f_m + self._forget_bias)
+        g_tp = torch.tanh(g_xp + g_m)
+
+        delta_c = i_t * g_t
+        c_new = f_t * c_t + delta_c
+        delta_m = i_tp * g_tp
+        m_new = f_tp * m_t + delta_m
+
+        mem = torch.cat((c_new, m_new), 1)
+        o_t = torch.sigmoid(o_x + o_h + self.conv_o(mem))
+        h_new = o_t * torch.tanh(self.conv_last(mem))
+        return h_new, c_new, m_new, delta_c, delta_m
+
+
+@model_builder.register_model('ForcedSTRNN')
+class ForcedSTRNN(pl.LightningModule):
+    def __init__(
+        self,
+        num_layers,
+        num_hidden,
+        img_channel,
+        act_channel,
+        init_cond_channel,
+        static_channel,
+        out_channel,
+        img_width,
+        device,
+        filter_size=5,
+        stride=1,
+    ):
+        super().__init__()
+
+        self.input_channel = img_channel
+        self.action_channel = act_channel
+        self.init_cond_channel = init_cond_channel
+        self.static_channel = static_channel
+        self.frame_channel = img_channel
+        self.num_layers = num_layers
+        self.num_hidden = num_hidden
+        self.device = device
+        self.out_channel = out_channel
+        self.decouple_loss = None
+        cell_list = []
+
+        for i in range(num_layers):
+            in_channel = self.frame_channel if i == 0 else num_hidden[i - 1]
+            cell_list.append(ActionSTLSTMCell(
+                in_channel, act_channel, num_hidden[i], filter_size, stride,
+            ))
+        self.cell_list = nn.ModuleList(cell_list)
+        # Set up the output conv layer, which
+        self.conv_last = nn.Conv2d(
+            num_hidden[num_layers - 1],
+            self.out_channel,
+            kernel_size=1,
+            bias=False
+        )
+        adapter_num_hidden = num_hidden[0]
+        self.adapter = nn.Conv2d(
+            adapter_num_hidden,
+            adapter_num_hidden,
+            kernel_size=1,
+            bias=False
+        )
+
+        self.memory_encoder = nn.Conv2d(
+            self.init_cond_channel, num_hidden[0], kernel_size=1, bias=False
+        )
+        # Should we encode different layers separately?
+        self.cell_encoder = nn.Conv2d(
+            self.static_channel, num_hidden[0], kernel_size=1, bias=False
+        )
+
+    def update_state(self, state):
+        out_shape = (state.shape[0], state.shape[1], -1)
+        return F.normalize(self.adapter(state).view(out_shape), dim=2)
+
+    def calc_decouple_loss(self, c, m):
+        return torch.mean(torch.abs(torch.cosine_similarity(c, m, dim=2)))
+
+    def forward(self, forcings, init_cond, static_inputs):
+        # Input shape:
+        #   (batch, length, channel, height, width)
+        batch, timesteps, channels, height, width = forcings.shape
+
+        # Initialize list of states
+        next_frames = []
+        h_t = []
+        c_t = []
+        delta_c_list = []
+        delta_m_list = []
+        decouple_loss = []
+        for i in range(self.num_layers):
+            zeros = torch.zeros([batch, self.num_hidden[i], height, width]).to(self.device)
+            h_t.append(zeros)
+            c_t.append(zeros)
+            delta_c_list.append(zeros)
+            delta_m_list.append(zeros)
+
+        # Initialize memory state and first layer cell state
+        # Note: static_inputs and init_cond should have length=1
+        # TODO: Fix this requirement
+        memory = self.memory_encoder(init_cond[:, 0])
+        # TODO: Should we encode all layer cell states?
+        c_t[0] = self.cell_encoder(static_inputs[:, 0])
+
+        # First input is the initial condition
+        x = init_cond[:, 0]
+        for t in range(timesteps):
+            a = forcings[:, t]
+            h_t[0], c_t[0], memory, dc, dm = self.cell_list[0](x, a, h_t[0], c_t[0], memory)
+            delta_c_list[0] = self.update_state(dc)
+            delta_m_list[0] = self.update_state(dm)
+
+            for i in range(1, self.num_layers):
+                h_t[i], c_t[i], memory, dc, dm = self.cell_list[i](h_t[i - 1], a, h_t[i], c_t[i], memory)
+                delta_c_list[i] = self.update_state(dc)
+                delta_m_list[i] = self.update_state(dm)
+
+            x = self.conv_last(h_t[-1]) + x
+            next_frames.append(x)
+            # decoupling loss
+            for i in range(0, self.num_layers):
+                decouple_loss.append(self.calc_decouple_loss(delta_c_list[i], delta_m_list[i]))
+
+        self.decouple_loss = torch.mean(torch.stack(decouple_loss, dim=0))
+        # Stack to: [batch, length, channel, height, width]
+        next_frames = torch.stack(next_frames, dim=1)
+        return next_frames
+
+    def training_step(self, train_batch, train_batch_idx):
+        forcing, state, params, target = train_batch
+        y_hat = self(forcing, state, params).squeeze()
+        loss = self.loss_fun(y_hat, target)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, val_batch, val_batch_idx):
+        forcing, state, params, target = val_batch
+        y_hat = self(forcing, state, params).squeeze()
+        y_hat = self(target).squeeze()
+        loss = self.loss_fun(y_hat, target)
+        self.log('val_loss', loss)
+        return loss
+
+    def configure_optimizers(
+        self,
+        opt=torch.optim.AdamW,
+    ):
+        optimizer = opt(self.model.parameters(), lr=0.001)
+        #scheduler = ExponentialLR(optimizer, gamma=0.9)
+        scheduler = OneCycleLR(optimizer, max_lr=0.001,
+                total_steps=self.steps_per_epoch * self.max_epochs)
+        return [optimizer], [scheduler]
+
+    def configure_loss(self, loss_fun=DWSE):
+        self.loss_fun = loss_fun
+
+
+
 @model_builder.register_model('BaseLSTM')
 class BaseLSTM(pl.LightningModule):
     """
